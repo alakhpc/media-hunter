f368d130d7743dbd682fc21a828f5f72739ef18c
Alakh <36898190+alakhpc@users.noreply.github.com>
2022-06-12 14:03:22 +0400

    Add layout and theme colors

--- apps/web-app/tailwind.config.js
+++ apps/web-app/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        theme: "#151828",
+      },
+    },
   },
   plugins: [],
 };
