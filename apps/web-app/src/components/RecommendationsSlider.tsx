f5778a8e5ffa87f4034f4923c9a1b162db313eb9
Alakh <36898190+alakhpc@users.noreply.github.com>
2022-06-17 02:30:47 +0400

    Handle no recs/cast

--- apps/web-app/src/components/RecommendationsSlider.tsx
+++ apps/web-app/src/components/RecommendationsSlider.tsx
@@ -8,39 +8,43 @@ interface RecommendationsSliderProps {
 }
 
 const RecommendationsSlider = ({
-  recommendations,
+  recommendations: recs,
 }: RecommendationsSliderProps) => {
   return (
     <div className="-m-2 flex flex-row space-x-3 overflow-x-auto p-2 scrollbar-hide">
-      {recommendations.map(({ type, id, title, poster, rating, year }, i) => (
-        <Link key={i} href={`/${type}/${id}`}>
-          <a>
-            <div className="flex cursor-pointer flex-col space-y-3">
-              <div className="w-32 shrink-0 rounded-lg shadow-sm transition duration-200 hover:scale-105 md:w-36">
-                <Image
-                  src={poster || "https://http.cat/404"}
-                  layout="responsive"
-                  width={2}
-                  height={3}
-                  alt={`${title} poster`}
-                  className="rounded-lg"
-                  sizes="(min-width: 768px) 144px, 128px"
-                />
-              </div>
-              <div className="flex w-32 flex-col md:w-36">
-                <div>{title}</div>
-                <div className="flex flex-row justify-between text-sm text-graytext">
-                  <div>{year}</div>
-                  <div className="flex flex-row items-center justify-between space-x-1">
-                    <div>{rating}</div>
-                    <HiStar className="h-5 w-5" />
+      {!(recs.length > 0) ? (
+        <div>No recommendations :(</div>
+      ) : (
+        recs.map(({ type, id, title, poster, rating, year }, i) => (
+          <Link key={i} href={`/${type}/${id}`}>
+            <a>
+              <div className="flex cursor-pointer flex-col space-y-3">
+                <div className="w-32 shrink-0 rounded-lg shadow-sm transition duration-200 hover:scale-105 md:w-36">
+                  <Image
+                    src={poster || "https://http.cat/404"}
+                    layout="responsive"
+                    width={2}
+                    height={3}
+                    alt={`${title} poster`}
+                    className="rounded-lg"
+                    sizes="(min-width: 768px) 144px, 128px"
+                  />
+                </div>
+                <div className="flex w-32 flex-col md:w-36">
+                  <div>{title}</div>
+                  <div className="flex flex-row justify-between text-sm text-graytext">
+                    <div>{year}</div>
+                    <div className="flex flex-row items-center justify-between space-x-1">
+                      <div>{rating}</div>
+                      <HiStar className="h-5 w-5" />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </a>
-        </Link>
-      ))}
+            </a>
+          </Link>
+        ))
+      )}
     </div>
   );
 };
