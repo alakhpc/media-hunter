69575eccc0e084422837e7e257a81254fcdd3074
alakhpc <36898190+alakhpc@users.noreply.github.com>
2022-10-26 00:08:48 -0400

    Upgrade deps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import Layout from "@/components/Layout";
 import { withTRPC } from "@trpc/next";
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { DefaultSeo } from "next-seo";
 import { SEO } from "next-seo.config";
-import type { AppProps } from "next/app";
+import type { AppType } from "next/app";
 import "tailwindcss/tailwind.css";
 import { AppRouter } from "./api/trpc/[trpc]";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
   return (
     <SessionProvider session={session}>
       <Layout>
@@ -16,7 +20,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
       </Layout>
     </SessionProvider>
   );
-}
+};
 
 export default withTRPC<AppRouter>({
   config() {
