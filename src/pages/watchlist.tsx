837b32c5334883767151b485d2fc99ac4adc0822
alakhpc <36898190+alakhpc@users.noreply.github.com>
2022-12-02 13:36:27 -0500

    Fix watchlist when not logged in

--- src/pages/watchlist.tsx
+++ src/pages/watchlist.tsx
@@ -1,9 +1,11 @@
 import MediaPoster from "@/components/MediaPoster";
 import PosterGrid from "@/components/PosterGrid";
 import { trpc } from "@/lib/trpc";
+import { useSession } from "next-auth/react";
 import { NextSeo } from "next-seo";
 
 const Search = () => {
+  const { data: session } = useSession();
   const watchlistQuery = trpc.useQuery(["watchlist.user"]);
 
   return (
@@ -12,19 +14,25 @@ const Search = () => {
       <div className="mx-4 mt-5 flex flex-col space-y-4">
         <div className="text-3xl md:text-4xl">Your Watchlist</div>
         <hr />
-        <PosterGrid size="md">
-          {watchlistQuery.data ? (
-            watchlistQuery.data.length > 0 ? (
-              watchlistQuery.data.map((r, i) => (
-                <MediaPoster key={i} preload={false} border={false} {...r} />
-              ))
+        {session ? (
+          <PosterGrid size="md">
+            {watchlistQuery.data ? (
+              watchlistQuery.data.length > 0 ? (
+                watchlistQuery.data.map((r, i) => (
+                  <MediaPoster key={i} preload={false} border={false} {...r} />
+                ))
+              ) : (
+                <div className="whitespace-nowrap">
+                  Nothing in your watchlist :(
+                </div>
+              )
             ) : (
-              <div>Nothing in your watchlist :(</div>
-            )
-          ) : (
-            <div>Loading...</div>
-          )}
-        </PosterGrid>
+              <div>Loading...</div>
+            )}
+          </PosterGrid>
+        ) : (
+          <div>Please login to view your watchlist</div>
+        )}
       </div>
     </>
   );
