8919ec48866daa716e4753a654924cdbecbbce89
alakhpc <36898190+alakhpc@users.noreply.github.com>
2024-05-29 15:17:46 -0400

    Remove pal link

--- src/components/CastSlider.tsx
+++ src/components/CastSlider.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 
 interface CastSliderProps {
   cast: { id: number; name: string; image: string | null; character: string }[];
@@ -12,28 +11,28 @@ const CastSlider = ({ cast }: CastSliderProps) => {
         <div>No cast :(</div>
       ) : (
         cast.map(({ id, name, image, character }, i) => (
-          <Link key={i} href={`/pal/${id}`}>
-            <a>
-              <div className="flex cursor-pointer flex-col space-y-3">
-                <div className="w-32 shrink-0 rounded-lg shadow-sm transition duration-200 hover:scale-105 md:w-36">
-                  <Image
-                    unoptimized
-                    src={image || "https://http.cat/404"}
-                    layout="responsive"
-                    width={2}
-                    height={3}
-                    alt={`${name} poster`}
-                    className="rounded-lg"
-                    sizes="(min-width: 768px) 144px, 128px"
-                  />
-                </div>
-                <div className="flex w-32 flex-col md:w-36">
-                  <div>{name}</div>
-                  <div className="text-sm text-graytext">{character}</div>
-                </div>
-              </div>
-            </a>
-          </Link>
+          // <Link key={i} href={`/pal/${id}`}>
+          //   <a>
+          <div key={i} className="flex flex-col space-y-3">
+            <div className="w-32 shrink-0 rounded-lg shadow-sm transition duration-200 hover:scale-105 md:w-36">
+              <Image
+                unoptimized
+                src={image || "https://http.cat/404"}
+                layout="responsive"
+                width={2}
+                height={3}
+                alt={`${name} poster`}
+                className="rounded-lg"
+                sizes="(min-width: 768px) 144px, 128px"
+              />
+            </div>
+            <div className="flex w-32 flex-col md:w-36">
+              <div>{name}</div>
+              <div className="text-sm text-graytext">{character}</div>
+            </div>
+          </div>
+          //   </a>
+          // </Link>
         ))
       )}
     </div>
